d9d07918661291b7e97269566e1ef363f80f6bea
Stackashu <ashu12141214@gmail.com>
2024-05-08 17:03:47 +0530

    added one more hollow code

---
+++ square.java
@@ -0,0 +1,32 @@
+class solidSquare{
+    void draw(int size){
+    for(int i = 1; i <= size;i++){
+        for ( int j = 1; j <= size ; j++ ){
+        System.out.print(" * ");
+        } 
+        System.out.print("\n");
+    }
+    }
+}
+class hollowSquare{
+    void draw(int size){
+    for(int i = 1; i <= size;i++){
+        for ( int j = 1; j <= size ; j++ ){
+            if((i == 1 || j == size) || ( j == 1 || i == size) ){
+                System.out.print("*" + " " );
+            }else {
+                System.out.print(" " + " " );
+            }
+        } 
+        System.out.print("\n");
+    }
+    }
+}
+
+public class square {
+    public static void main(String[] args) {
+       
+       hollowSquare dabba = new hollowSquare();
+       dabba.draw(5);
+    }
+}
