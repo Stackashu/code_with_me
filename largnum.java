fe09835db94f0392364e92755595fc531ea148e8
Stackashu <ashu12141214@gmail.com>
2024-05-08 15:45:32 +0530

    new code genreated

--- largnum.java
+++ largnum.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class largnum{
     public static void main(String[] args) {
-        System.out.println("hello world !");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a = scan.nextInt();
+        System.out.println("Enter second number");
+        int b = scan.nextInt();
+        System.out.println("Enter third number");
+        int c = scan.nextInt();
+
+        if (a>b && a>c){
+            System.out.println(" your number " + a + " is greater ");
+        }else if(a<b && b>c){
+            System.out.println("your number " + b +" is greater  ");
+        }else {
+            System.out.println("your number " + c + " is greater " );
+        }
+        
     }
 }
