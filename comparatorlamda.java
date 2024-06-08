2f88e626c51aabfa6a871efb05a1e5b1f30587a5
Stackashu <ashu12141214@gmail.com>
2024-06-08 15:17:36 +0530

    lambda function as comparator in list

---
+++ comparatorlamda.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+public class comparatorlamda {
+    public static void main(String[] args) {
+        List<Integer> ls = new ArrayList<>();
+        ls.add(2);
+        ls.add(30);
+        ls.add(1);
+        ls.add(0);
+        ls.add(32);
+        //you will get the list in increasing order....
+        Collections.sort(ls,(a,b) -> a-b);
+        System.out.println(ls);
+        //you will get the list in decreasing order....
+        Collections.sort(ls,(a,b) -> b-a);
+        System.out.println(ls);
+        
+    }
+}
